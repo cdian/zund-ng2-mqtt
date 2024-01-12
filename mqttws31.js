01ec48975da3fef947e828ba42cfa521b23976ae
Cehic Dian <dian.cehic@zund.com>
2024-01-12 13:23:58 +0100

    const

--- mqttws31.js
+++ mqttws31.js
@@ -80,9 +80,9 @@ function onMessageArrived(message) {
  * @namespace Paho.MQTT 
  */
 
-// if (typeof Paho === "undefined") {
-	Paho = {};
-// }
+
+const Paho = {};
+
 
 Paho.MQTT = (function (global) {
 
